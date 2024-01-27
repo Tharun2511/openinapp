b7c76625abb0f0d3ffa468e96bcefe61eb1804ff
Tharun <gtharun2511@gmail.com>
2024-01-27 19:27:38 +0530

    taskPriorityLogic

--- cronLogic/taskPriorityLogic.js
+++ cronLogic/taskPriorityLogic.js
@@ -2,10 +2,20 @@ const cron = require("node-cron");
 const Task = require("../models/taskModel");
 
 const setTaskPriority = async () => {
-    cron.schedule("* * * * * *", () => {
+    cron.schedule("* 0,6,12,18 * * *", () => {
         Task.updateMany(
             {},
-            { $set: { priority: Date.now("$dueDate") - Date.now() } },
+            {
+                $set: {
+                    priority: () => {
+                        const days = Date.parse("$dueDate") - Date.now();
+                        if (days <= 0) return 0;
+                        if (days == 1 && days == 2) return 1;
+                        if (days == 3 && days == 4) return 2;
+                        return 3;
+                    },
+                },
+            },
             {
                 multi: true,
             }
